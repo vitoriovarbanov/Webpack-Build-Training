ab41807ec9ad2dd99873fe1d4bb0fd821600d480
vitoriovarbanov <vitorio.0405@hotmail.com>
2022-05-25 13:40:35 +0300

    added html webpack plugin/devServer and created initial dev builld with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        static: {
+           directory: path.resolve(__dirname, 'dist')
+        },
+        open: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(jsx|js)$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+          favicon: './public/favicon.ico'
+        })
+      ]
 }
